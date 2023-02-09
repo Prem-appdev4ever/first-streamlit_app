716fcd1037aa4cc20914410af91e4d00c4e223d7
Prem-appdev4ever <124648433+Prem-appdev4ever@users.noreply.github.com>
2023-02-09 11:36:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,18 @@ streamlit.title('FAVOURITES')
 
 
 
-streamlit.header ('🥣 🥗 🐔 🥑Breaky Menu🥣 🥗 🐔 🥑')
+streamlit.header ('🥣 🥗 🐔 Breaky Menu🥣 🥗 🐔')
+
+
 streamlit.text('🥗Bread & Omlette')
 streamlit.text('🐔Hungry Jacks Burger & happy meal')
 streamlit.text ('🥣Mc donalds haapy meal🥣')
 
 
+
+
 streamlit.header('🥑Fruit Smoothie🥑')
+
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
